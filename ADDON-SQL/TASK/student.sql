b11bccb21ee36c5e48a5637da8802592708e8eea
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-16 14:20:09 +0530

    Update student.sql

--- ADDON-SQL/TASK/student.sql
+++ ADDON-SQL/TASK/student.sql
@@ -61,36 +61,3 @@ mysql> select * from student;
 |  76546 | sahana | bsc ct | mysql  |        NULL |
 |  76547 | pooja  | bsc ct | mysql  |        NULL |
 +--------+--------+--------+--------+-------------+
-
-mysql> create table Course(
-    -> Coursefee int
-    -> );
-Query OK, 0 rows affected (0.04 sec)
-
-mysql> insert into course values(10000);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> insert into course values(20000);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> insert into course values(30000);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> insert into course values(40000);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> insert into course values(50000);
-Query OK, 1 row affected (0.01 sec)
-
-mysql> select*
-    -> from course;
-+-----------+
-| Coursefee |
-+-----------+
-|     10000 |
-|     20000 |
-|     30000 |
-|     40000 |
-|     50000 |
-+-----------+
-5 rows in set (0.00 sec)
