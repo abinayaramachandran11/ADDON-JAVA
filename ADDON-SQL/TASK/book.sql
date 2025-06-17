2de8362cdcc79eaeb79b3c53adf88c99bdc77d63
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-17 20:55:16 +0530

    Update book.sql

--- ADDON-SQL/TASK/book.sql
+++ ADDON-SQL/TASK/book.sql
@@ -1,3 +1,5 @@
+Books by keyword using LIKE in sql?
+
 mysql> use book
 Database changed
 mysql> create table book(
@@ -5,3 +7,24 @@ mysql> create table book(
     -> bookname varchar(20),
     -> price int,
     -> );
+mysql> insert into book values(3639,'tree',450);
+mysql> insert into book values(3633,'moon',475);
+mysql> insert into book values(3633,'sun',450);
+mysql> insert into book values(3633,'rainbow',480);
+mysql> select*from book;
++--------+----------+-------+
+| bookno | bookname | price |
++--------+----------+-------+
+|   3639 | tree     |   450 |
+|   3633 | moon     |   475 |
+|   3633 | sun      |   450 |
+|   3633 | rainbow  |   480 |
++--------+----------+-------+
+
+mysql> select*from book
+    -> where bookname like 'm%';
++--------+----------+-------+
+| bookno | bookname | price |
++--------+----------+-------+
+|   3633 | moon     |   475 |
++--------+----------+-------+
