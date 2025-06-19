4d84500f75545dec0794db31cdbc67aba521b5c9
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-19 14:18:20 +0530

    Add files via upload

--- ADDON-SQL/DAY-07/TASK/updatecourse.sql
+++ ADDON-SQL/DAY-07/TASK/updatecourse.sql
@@ -1,23 +1,13 @@
+TASK 3
 mysql> create table Course(
     -> Coursefee int
     -> );
-Query OK, 0 rows affected (0.04 sec)
 
 mysql> insert into course values(10000);
-Query OK, 1 row affected (0.01 sec)
-
 mysql> insert into course values(20000);
-Query OK, 1 row affected (0.01 sec)
-
 mysql> insert into course values(30000);
-Query OK, 1 row affected (0.01 sec)
-
 mysql> insert into course values(40000);
-Query OK, 1 row affected (0.01 sec)
-
 mysql> insert into course values(50000);
-Query OK, 1 row affected (0.01 sec)
-
 mysql> select*
     -> from course;
 +-----------+
@@ -29,7 +19,7 @@ mysql> select*
 |     40000 |
 |     50000 |
 +-----------+
-
+    
 mysql> update course
     -> set coursefee=60000
     -> where coursefee=50000;
