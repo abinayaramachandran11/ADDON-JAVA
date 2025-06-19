ad50d2e020d48c97aef4e5e553e33710566ddc0e
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-19 20:33:50 +0530

    Add files via upload

---
+++ ADDON-SQL/DAY-10/TASK/arg-sal.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Employee (
+  EmpId int,
+  Ename varchar(15),
+  Edept varchar(10),
+  Esalary int
+);
+
+INSERT INTO Employee(EmpId,Ename,Edept,Esalary) VALUES (1, 'Abi', 'Accounting', 550000);
+INSERT INTO Employee(EmpId,Ename,Edept,Esalary) VALUES (2, 'Pooja', 'Salesman', 20000);
+INSERT INTO Employee(EmpId,Ename,Edept,Esalary) VALUES (3, 'Sujitha', 'Clerk', 40000);
+INSERT INTO Employee(EmpId,Ename,Edept,Esalary) VALUES (4, 'Nive', 'Manager', 45000);
+INSERT INTO Employee(EmpId,Ename,Edept,Esalary) VALUES (5, 'Madhu', 'Clerk', 40000);
+
+SELECT Edept, avg(Esalary) as average_salary
+FROM Employee
+group by Edept;
+
++------------+----------------+
+| Edept      | average_salary |
++------------+----------------+
+| Accounting |     55000.0000 |
+| Salesman   |     20000.0000 |
+| Clerk      |     40000.0000 |
+| Manager    |     45000.0000 |
++------------+----------------+
