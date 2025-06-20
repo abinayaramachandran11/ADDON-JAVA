b16729a2663fb3e95f465b64d1abf649c65e5ca8
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-20 14:42:25 +0530

    Create Overloading.java

---
+++ ADDON-JAVA/TASK/Overloading.java
@@ -0,0 +1,24 @@
+package task;
+
+class methods{
+	 public void sum(int a,int b) {
+		 System.out.println("Enter the value int a:"+a);
+		 System.out.println("Enter the value int b:"+b);
+		 System.out.println("total sum:"+(a+b));
+	 }
+    public void sum(double a,double b) {
+	     System.out.println("Enter the value double a:"+a);
+	     System.out.println("Enter the value double b:"+b);
+	     System.out.println("total sum:"+(a+b));
+     }
+}
+
+public class Overloading {
+
+	public static void main(String[] args) {
+		methods cv=new methods();
+		cv.sum(10, 20);
+		cv.sum(20.0, 30.3);
+	}
+
+	}
