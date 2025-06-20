6277a15ab9ae1da4bd256ed42b33e769d2bcdc24
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-20 19:49:18 +0530

    Create evennum.java

---
+++ ADDON-JAVA/TASK/evennum.java
@@ -0,0 +1,13 @@
+Print all even numbers between 1 and 100?
+	
+package program;
+import java.util.*;
+public class evennumbers {
+	public static void main(String[] args) {
+		for(int a=1;a<=100;a++) {
+            if(a%2==0) {
+            System.out.println(a);
+}
+	}
+}
+
