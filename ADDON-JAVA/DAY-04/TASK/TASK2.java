c4d4efad54dfeebfa9e3fdc86b354a8effe333df
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-20 19:37:37 +0530

    Add files via upload

---
+++ ADDON-JAVA/DAY-04/TASK/TASK2.java
@@ -0,0 +1,16 @@
+package program;
+import java.io.*;
+public class Task2 {
+     public static void main(String[] args) {
+     String filename="C:\\Users\\TEMP\\Desktop\\Java\\java\\Demo.txt";
+    			 try(BufferedReader br=new BufferedReader(new FileReader(filename))) {
+    				 String Line;
+    				 if((Line=br.readLine()) !=null) {
+    					 System.out.println(Line);
+    				 }
+    			 }
+    			 catch(Exception e) {
+    				 System.out.println("File not found");
+    			 }
+     }
+}
