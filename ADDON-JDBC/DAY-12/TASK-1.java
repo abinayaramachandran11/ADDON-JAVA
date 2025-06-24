0a68e0e27115b10b6fb3c387ea719e67998a39cf
abinayaramachandran11 <203629054+abinayaramachandran11@users.noreply.github.com>
2025-06-24 18:43:58 +0530

    Create TASK-1.java

---
+++ ADDON-JDBC/DAY-12/TASK-1.java
@@ -0,0 +1,11 @@
+package Mysqlprogram;
+import java.sql.*;
+public class Task1 {
+	public static void main(String[] args) throws Exception {
+	String url = "jdbc:mysql://localhost:3306/employee";
+	String username = "root";
+	String password = "ksql@";
+	Connection connect = DriverManager.getConnection(url, username, password);
+	System.out.println("Driver Loaded");
+}
+}
